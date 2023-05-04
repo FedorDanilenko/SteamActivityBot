6b36644c9b5eb73d190f5aa58a14a12643713822
Fedor Danilenko <divine.world.mail@gmail.com>
2023-05-04 15:35:13 +0400

    Create project

---
+++ src/main/java/jm/bot/steamactivitybot/SteamActivityBotApplication.java
@@ -0,0 +1,13 @@
+package jm.bot.steamactivitybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamActivityBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamActivityBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jm/bot/steamactivitybot/SteamActivityBotApplicationTests.java
@@ -0,0 +1,13 @@
+package jm.bot.steamactivitybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamActivityBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
